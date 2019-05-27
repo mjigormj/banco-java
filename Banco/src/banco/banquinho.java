d350bdaa288feafd61e0e9fb115e4dec06eb2258
mjigormj <igorcruzsilva@gmail.com>
2019-05-27 20:26:18 -0300

    hgfi

--- Banco/src/banco/banquinho.java
+++ Banco/src/banco/banquinho.java
@@ -3,33 +3,39 @@ package banco;
 import java.util.Scanner;
 
 public class banquinho {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Banco banco = new Banco();
-        Scanner scan= new Scanner (System.in);
+        Scanner scan = new Scanner(System.in);
         int option;
         do {
             System.out.println("=========== Selecione uma opção ===========");
-            System.out.println("1 - Efetuar depósito");
-            System.out.println("3 - Encerrar");
+            System.out.println("1 - Criar conta");
+            System.out.println("2 - Efetuar depósito");
+            System.out.println("3 - Efetuar saque");
+            System.out.println("4 - Extrato");
+            System.out.println("5 - Encerrar");
             System.out.println("opção: ");
             option = scan.nextInt();
-        }
-        
-        
-        
-        
-        System.out.print("Digite seu nome: ");
-        String nomeDoCliente = scan.next();
-        
-        System.out.print("Digite o número da sua conta: ");
-        int numero = scan.nextInt();
-             
-        System.out.println("Digite o valor para realizar o saque: ");
-        Double valorASerSacado = scan.nextDouble();
-        banco.sacar(valorASerSacado);
-        
-        System.out.println("Digite o valor para realizar um deposito: ");
-        Double valorASerDepositado = scan.nextDouble();
-        banco.depositar(valorASerDepositado);
+
+            if (option == 1) {
+                System.out.print("Digite seu nome: ");
+                String nomeDoCliente = scan.next();
+                System.out.print("Digite o número da sua conta: ");
+                int numero = scan.nextInt();
+            } else if (option == 3) {
+                banco.sacar(2);
+
+            } else if (option == 4) {
+                System.out.println(nomeDoCliente);
+                System.out.println(numero);
+                System.out.println(saldo);
+            } else if (option == 5) {
+                System.exit(0);
+            }
+
+        } while (option != 0);
+
     }
+
 }
